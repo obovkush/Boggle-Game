029a55c4000a380753326f29de48cfbe28fa2ad0
OlegBovkush <obovkush@gmail.com>
2022-03-07 02:58:15 +1000

    Release 0 add word to card

--- src/components/ButtonAddWord.jsx
+++ src/components/ButtonAddWord.jsx
@@ -1,13 +1,29 @@
 import { Button } from 'antd';
 
 const NewWordButton = () => {
-
+  const cellAll = document.querySelectorAll('.ant-col');
+  let newWord = '';
+  function classСheck(cell) {
+    return cell.classList.contains('cell-checked');
+  }
+  function clickOn(event) {
+    event.preventDefault();
+    const cell = event.target;
+    if (classСheck(cell)) {
+      cell.innerText === 'Q' ? newWord += 'QU' : newWord += cell.innerText;
+    }
+  }
+  cellAll.forEach((cell) => {
+    cell.addEventListener('click', clickOn);
+  })
+  
   const onButtonClick = (event) => {
     event.preventDefault();
-    const newWord = 'word'
+
     let cardInfo = document.querySelector('.ant-card-body')
     cardInfo = document.querySelector('.ant-card-body');
-    cardInfo.innerHTML += `<span><strong>&nbsp;${newWord}&nbsp<strong></span>`
+    cardInfo.innerHTML += `<span><strong>&nbsp${newWord.toLowerCase()}&nbsp<strong></span>`
+    newWord = '';
   }
 
   return (
